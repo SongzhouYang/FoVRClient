822906ee6f3d65233c6d5bd3201cc3c18777b024
Songzhou Yang <yangsongzhou@outlook.com>
2021-11-26 14:58:52 +0800

    init

---
+++ Assets/TCPClient.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.Net.Sockets;
+using UnityEngine;
+
+public class TCPClient : MonoBehaviour {
+	internal Boolean socketReady = false;
+	TcpClient mySocket;
+	NetworkStream theStream;
+	StreamWriter theWriter;
+	StreamReader theReader;
+	String Host = "localhost";
+	Int32 Port = 8888;
+
+	//StreamWriter writer = new StreamWriter("data.txt", true);
+	int timer = 0;
+
+	void Start () {
+		// setupSocket ();
+	}
+
+	void Update () {
+		try {
+			Vector3 ray_origin = FoveInterface.GetGazeConvergence ().ray.origin;
+			Vector3 ray_direction = FoveInterface.GetGazeConvergence ().ray.direction;
+			Vector3 position = FoveInterface.GetHMDPosition ();
+			Quaternion rotation = FoveInterface.GetHMDRotation ();
+			Vector3 resultV = rotation * (ray_origin + ray_direction + position);
+			string result = ray_origin.ToString() + ray_direction.ToString() + position.ToString() + rotation.ToString() + '\n';
+			// Debug.Log (result.ToString());
+			timer += 1;
+			if (timer == 12) {
+				timer = 0;
+				//Debug.Log (result);
+				//writer.WriteLine(result);
+				//writer.Close();
+				File.AppendAllText("data.txt", result);
+			}
+			// readSocket ();
+		} catch { }
+	}
+	// **********************************************
+	public void setupSocket () {
+		try {
+			mySocket = new TcpClient (Host, Port);
+			theStream = mySocket.GetStream ();
+			theWriter = new StreamWriter (theStream);
+			theReader = new StreamReader (theStream);
+			socketReady = true;
+		} catch (Exception e) {
+			Debug.Log ("Socket error: " + e);
+		}
+	}
+
+	public void writeSocket (string theLine) {
+		if (!socketReady)
+			return;
+		String foo = theLine + "\r\n";
+		theWriter.Write (foo);
+		theWriter.Flush ();
+	}
+
+	public String readSocket () {
+		if (!socketReady)
+			return "";
+		try {
+			return theReader.ReadLine ();
+		} catch (Exception e) {
+			return "";
+		}
+	}
+
+	public void closeSocket () {
+		if (!socketReady)
+			return;
+		theWriter.Close ();
+		theReader.Close ();
+		mySocket.Close ();
+		socketReady = false;
+	}
+}
+// end class TCPClient
---
+++ README.md
@@ -0,0 +1,19 @@
+# FoVR: Attention-based VR Streaming through Bandwidth-limited Wireless Networks
+
+The client framework of FoVR.
+
+This project requires following packages:
+1. The [FoveVR Unity Plugin](https://github.com/FoveHMD/UnityPlugin).
+2. The [UMP package](https://assetstore.unity.com/packages/tools/video/ump-win-mac-linux-webgl-49625), which is removed due to the commercial license.
+
+If you find this project useful, please cite the paper:
+```
+@inproceedings{yang2019fovr,
+  title={Fovr: Attention-based VR streaming through bandwidth-limited wireless networks},
+  author={Yang, Songzhou and He, Yuan and Zheng, Xiaolong},
+  booktitle={2019 16th Annual IEEE International Conference on Sensing, Communication, and Networking (SECON)},
+  pages={1--9},
+  year={2019},
+  organization={IEEE}
+}
+```
